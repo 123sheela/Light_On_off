6d95b6111b779c844fae969ff6e85bc5d546c10f
Sheela <91262191+123sheela@users.noreply.github.com>
2024-03-01 12:32:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+
+var isBulbOn = false;
+
 function change() {
     var img = document.getElementById("bulb");
-    console.log("Current image source: " + img.src);
-    if (img.src.includes("off")) {
-        console.log("Changing to ON");
+    
+
+    isBulbOn = !isBulbOn;
+
+    if (isBulbOn) {
         img.src = "./assets/on.jpg";
     } else {
-        console.log("Changing to OFF");
         img.src = "./assets/off.jpg";
     }
-    console.log("New image source: " + img.src);
 }
